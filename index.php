67201881b2a2db860325f85b9f4b58f2188e02bd
Valeria <valeriasilva.info@eter.org.br>
2018-10-31 17:28:33 -0300

    PROVA

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+<body>
+
+    <form action="Upload.php" method="post" enctype="multipart/form-data">
+    Por favor, selecione a sua foto:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="Enviar foto">
+</form>
+
+
+
+    </body>
+</html>
